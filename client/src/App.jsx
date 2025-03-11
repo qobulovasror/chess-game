8dfd0ba90091f9ef0f2d07201ca6a3001cde030e
qobulovasror <qobulovasror0@gmail.com>
2025-03-11 18:21:50 +0500

    added shadcn ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,13 @@
-import { Button } from '@/components/ui/button';
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@/components/ui/alert-dialog';
+import { Button } from "@/components/ui/button"
+
 
 function App() {
+ 
   return (
-    <>
-      <AlertDialog>
-        <AlertDialogTrigger>Open</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+    <><Button variant="outline">Button</Button>
+
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
-import path from "path";
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
