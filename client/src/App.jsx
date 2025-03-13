eaa39a3e1093b830b47409e91f2cc5e39497342f
qobulovasror <qobulovasror0@gmail.com>
2025-03-13 09:25:51 +0500

    added reac-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import Game from './Game'
-import { ThemeProvider } from "@/components/theme-provider"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Game from "./Game";
+import { ThemeProvider } from "@/components/theme-provider";
+import Home from "./Home";
+// import Dashboard from "./Dashboard";
+// import CreateOrJoin from "./CreateOrJoin";
+// import Project from "./Project";
+// import Multiplyer from "./Multiplyer";
+// import PlayWithComp from "./PlayWithComp"; // Noto‘g‘ri yozilgan nomni to‘g‘rilandi
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-     <Game/>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Game />} />
+          {/* <Route path="/multiplayer" element={<Multiplyer />} /> */}
+          {/* <Route path="/playWithComp" element={<PlayWithComp />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />}>
+            <Route index element={<CreateOrJoin />} />
+            <Route path="game" element={<Project />} />
+          </Route>*/}
+        </Routes> 
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
